5d65de9a2701e51482e5d98ef064195c2b9d3d9c
Arnob Mukherjee <arnob8@gmail.com>
2025-01-06 13:28:41 +0530

    CHanges in data_trans_3 and the pkl files

--- src/components/data_trans_3.py
+++ src/components/data_trans_3.py
@@ -1,12 +1,8 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
 import numpy as np
 import pandas as pd
-from sklearn.compose import ColumnTransformer #used to create the pipeline
-from sklearn.impute import SimpleImputer # to impute missing values
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -16,6 +12,11 @@ from src.utils import save_object # jsut used for saving the pickle fil
 import sys # provides access to system specific parameter and funcs, used to interact with Python runtime env
 from dataclasses import dataclass
 import os
+
+print("version in data_trans_3")
+print("numpy",np.__version__)
+print("pandas",pd.__version__)
+
 
 class CompleteTransformer(BaseEstimator, TransformerMixin):
     """
@@ -27,7 +28,7 @@ class CompleteTransformer(BaseEstimator, TransformerMixin):
     def transform(self, df):
         # Create a copy to avoid modifying the original data
         df = df.copy()
-        try:       
+        try:
 ################################################
 #3.0 - Transforming the date and time columns to form PlannedDateTime & ArrivedDateTime
             print("Shape Before Transformation - 3.0 - Forming PlannedDateTime & ArrivedDateTime:",df.shape)
@@ -35,10 +36,10 @@ class CompleteTransformer(BaseEstimator, TransformerMixin):
                 df["Planned Date"] + " " + df["Planned Time"],
                 format="%d/%m/%Y %I:%M:%S %p",
             )
-            df["ArrivedDateTime"] = pd.to_datetime(
-                df["Arrival Date"] + " " + df["Arrival Time"],
-                format="%d/%m/%Y %I:%M:%S %p",
-            )
+            #df["ArrivedDateTime"] = pd.to_datetime(
+                #df["Arrival Date"] + " " + df["Arrival Time"],
+                #format="%d/%m/%Y %I:%M:%S %p",
+            #)
             print("Shape After Transformation - 3.0 - Forming PlannedDateTime & ArrivedDateTime:",df.shape)
 ################################################
 #3.1 - Label Encoding CarrierID,RelationID,CustomerID            
@@ -70,21 +71,13 @@ class CompleteTransformer(BaseEstimator, TransformerMixin):
                     "Order type",
                     "CustomerID",
                     "PlannedDateTime",
-                    "ArrivedDateTime",
+                    #"ArrivedDateTime",
                     "NumberOfOrders",
                 ]
             ]
             print("Shape After Transformation - 3.3 - Selecting only Required Columns:",df.shape)
-################################################
-#3.4 - Deriving the Target Variable
-        
-            # TimeDifference = ArrivedTime - PlannedTime in minutes
-            print("Shape Before Transformation - 3.4 - Derive Target Variable for Train:",df.shape)
-            df['Delay'] = (df['ArrivedDateTime'] - df['PlannedDateTime']).dt.total_seconds() / 60  # in minutes
-            df["TargetVariable"] = np.where(df["Delay"] >= 15, 1, 0)  # Target variable derived
-            logging.info("Target variable derived successfully.")
-            print("Shape After Transformation - 3.4 - Derive Target Variable for Train:",df.shape)
-               
+
+                     
 ################################################
 #3.5 - Removing Records with PlannedDateTime as NULL
     
@@ -302,7 +295,7 @@ class CompleteTransformer(BaseEstimator, TransformerMixin):
 #3.14 - Dropping all ID based columns
             print("Shape before Transformation - 3.14 - Dropping All ID Based column:",df.shape)
             # Calculate Order Density by Hour
-            columns_to_drop = ["Date","CarrierID","RelationID","Trip Nr","Order type","CustomerID","PlannedDateTime","ArrivedDateTime","Delay","Planned_Week"]
+            columns_to_drop = ["Date","CarrierID","RelationID","Trip Nr","Order type","CustomerID","PlannedDateTime","Planned_Week"]
             df = df.drop(columns=columns_to_drop)
             logging.info("3.14 - Dropped all ID based columns successfully.")
             print("Shape before Transformation - 3.14 - Dropping All ID Based column:",df.shape)
@@ -332,8 +325,8 @@ class CompleteTransformer(BaseEstimator, TransformerMixin):
                                 "Planned_Day_sin",
                                 "Planned_Week_freq",
                                 "Planned_Month_freq",
-                                "Planned_Week_cos",
-                                "TargetVariable"
+                                "Planned_Week_cos"
+                                #"TargetVariable"
                             ]
 
             # Select the specified columns
@@ -388,16 +381,79 @@ class DataTransformation3:
             train_df = pd.read_csv(train_data_path)
             test_df = pd.read_csv(test_data_path)
 
-            
+            #Step-1.5: Preprocessing which is just specific with regards 
+            # to model building and will not be required in preprocessor.pkl
+            # Preprocessing steps to derive the Target Variable [Remember, 
+            # it is not present in dataset]
+################################################
+#T.0 - Transforming the date and time columns to form PlannedDateTime & ArrivedDateTime
+            print("Shape Before Transformation - T.0 - Forming PlannedDateTime & ArrivedDateTime:",train_df.shape,test_df.shape)
+            train_df["PlannedDateTime"] = pd.to_datetime(
+                train_df["Planned Date"] + " " + train_df["Planned Time"],
+                format="%d/%m/%Y %I:%M:%S %p",
+            )
+            train_df["ArrivedDateTime"] = pd.to_datetime(
+                train_df["Arrival Date"] + " " + train_df["Arrival Time"],
+                format="%d/%m/%Y %I:%M:%S %p",
+            )
+
+            print("Shape Before Transformation - 3.0 - Forming PlannedDateTime & ArrivedDateTime:",train_df.shape,test_df.shape)
+            test_df["PlannedDateTime"] = pd.to_datetime(
+                test_df["Planned Date"] + " " + test_df["Planned Time"],
+                format="%d/%m/%Y %I:%M:%S %p",
+            )
+            test_df["ArrivedDateTime"] = pd.to_datetime(
+                test_df["Arrival Date"] + " " + test_df["Arrival Time"],
+                format="%d/%m/%Y %I:%M:%S %p",
+            )
+            print("Shape After Transformation - T.0 - Forming PlannedDateTime & ArrivedDateTime:",train_df.shape,test_df.shape)
+
+################################################
+#T.1 - Deriving the Target Variable
+        
+            # TimeDifference = ArrivedTime - PlannedTime in minutes
+            print("Shape Before Transformation - T.1 - Derive Target Variable for Train:",train_df.shape)
+            train_df['Delay'] = (train_df['ArrivedDateTime'] - train_df['PlannedDateTime']).dt.total_seconds() / 60  # in minutes
+            train_df["TargetVariable"] = np.where(train_df["Delay"] >= 15, 1, 0)  # Target variable derived
+            logging.info("Target variable derived successfully.")
+            print("Shape After Transformation - T.1- Derive Target Variable for Train:",train_df.shape)
+             
+             # TimeDifference = ArrivedTime - PlannedTime in minutes
+            print("Shape Before Transformation - T.1 - Derive Target Variable for Test:",test_df.shape)
+            test_df['Delay'] = (test_df['ArrivedDateTime'] - test_df['PlannedDateTime']).dt.total_seconds() / 60  # in minutes
+            test_df["TargetVariable"] = np.where(test_df["Delay"] >= 15, 1, 0)  # Target variable derived
+            logging.info("Target variable derived successfully.")
+            print("Shape After Transformation - T.1 - Derive Target Variable for Test:",test_df.shape)
+ 
+################################################
+#T.2- Dropping the columns = PlannedDateTime,ArrivedDateTime,Delay
+            to_drop = ["PlannedDateTime","ArrivedDateTime","Delay"]
+            train_df = train_df.drop(columns = to_drop,axis = 1)
+            test_df = test_df.drop(columns = to_drop,axis = 1)
+
+
+            #Step-2: Removing the Target Variable from the dataframe and sending it for preprocessing
+            target_column_name = "TargetVariable"
+            input_feature_train_df = train_df.drop(columns = [target_column_name],axis = 1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns = [target_column_name],axis = 1)
+            target_feature_test_df = test_df[target_column_name]    
             
             print("Information of DataFrame")
             print(train_df.info())
 
-            #Step-2: getting an object ready
+            #Step-2: Sending the DataFrame with Input Features for preprocessing
             preprocessing_obj = self.get_data_transformer_object()
-            train_df_final = preprocessing_obj.fit_transform(train_df)
-            test_df_final = preprocessing_obj.fit_transform(test_df)
-            # Step 3: Save the transformed data to CSV files
+            input_feature_train_df_final = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_df_final = preprocessing_obj.fit_transform(input_feature_test_df)
+
+            #Step-3: Rejoining the train_df_final & test_df_final back with the Target Variable
+            train_df_final = pd.concat([input_feature_train_df_final,target_feature_train_df],axis = 1)
+            test_df_final = pd.concat([input_feature_test_df_final,target_feature_test_df],axis = 1)
+
+
+            # Step 4: Save the transformed data to CSV files - this is just a precautionary measure
             logging.info("Saving transformed train and test datasets.")
             train_df_final.to_csv(
                 self.data_transformation_config3.train_data_final_path, index=False, header=True
@@ -406,21 +462,9 @@ class DataTransformation3:
             test_df_final.to_csv(
                 self.data_transformation_config3.test_data_final_path, index=False, header=True
             )
-            #Step 4: New Addition by Arnob - Dropping the Target Variable from Train and Test Sets finally created
-            #Commenting it - 04-Jan-2025 - Arnob
-            #target_column_name = "TargetVariable"
-            #input_feature_train_df = train_df_final.drop(columns = [target_column_name],axis = 1)
-            #target_feature_train_df = train_df_final[target_column_name]
+        
 
-            #input_feature_test_df = test_df_final.drop(columns = [target_column_name],axis = 1)
-            #target_feature_test_df = test_df_final[target_column_name]
-
-            #Step 5: Our dataframe is all messed up, so we will conver it into an array(easier for ML models to work)
-            #And , we will put the Target variable column at the very last
-            #train_arr = np.c_[input_feature_train_df, np.array(target_feature_train_df)]
-            #test_arr = np.c_[input_feature_test_df, np.array(target_feature_test_df)]
-
-            #Step 6: Saving the preprocessing object
+            #Step 5: Saving the preprocessing object
 
               #we write this function save_object in utils
             save_object(
